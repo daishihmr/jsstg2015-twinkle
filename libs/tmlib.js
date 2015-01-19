7ea8fa3fbf49075100e68dacbcc73d655d4fbb0c
daishihmr <daishi.hmr@gmail.com>
2015-01-19 14:53:55 +0900

    build

--- libs/tmlib.js
+++ libs/tmlib.js
@@ -7547,6 +7547,55 @@ tm.dom = tm.dom || {};
     });
 
 })();
+tm.asset = tm.asset || {};
+
+(function() {
+
+    tm.define("tm.asset.Font", {
+        superClass: "tm.event.EventDispatcher",
+
+        init: function(path, key) {
+            this.superInit();
+
+            var testElement = tm.dom.Element("body").create("span");
+            testElement.style
+                .set("color", "rgba(0, 0, 0, 0)")
+                .set("fontSize", "40px");
+            testElement.text = "QW@HhsXJ=/()あいうえお＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝";
+
+            var before = testElement.element.offsetWidth;
+
+            testElement.style
+                .set("fontFamily", "'{0}', 'monospace'".format(key));
+
+            var fontFaceStyleElement = tm.dom.Element("head").create("style");
+            fontFaceStyleElement.text = "@font-face { font-family: '{0}'; src: url({1}) format('truetype'); }".format(key, path);
+
+            var checkLoadFont = function() {
+                if (testElement.element.offsetWidth !== before) {
+                    testElement.remove();
+                    this.flare("load");
+                    console.debug("webfont loaded", path, key);
+                } else {
+                    setTimeout(checkLoadFont, 100);
+                }
+            }.bind(this);
+            setTimeout(checkLoadFont, 100);
+        },
+    });
+
+    tm.asset.Loader.register("ttf", function(path, key) {
+        return tm.asset.Font(path, key);
+    });
+    tm.asset.Loader.register("otf", function(path, key) {
+        return tm.asset.Font(path, key);
+    });
+    tm.asset.Loader.register("woff", function(path, key) {
+        return tm.asset.Font(path, key);
+    });
+
+})();
+
 /*
  * keyboard.js
  */
@@ -16114,37 +16163,37 @@ tm.ui = tm.ui || {};
      * @extends tm.display.Shape
      */
     tm.define("tm.ui.FlatButton", {
-        superClass: tm.display.Shape,
+        superClass: "tm.display.RoundRectangleShape",
 
         /**
          * @constructor
          */
         init: function(param) {
-            param.$safe({
-                width: 300,
-                height: 100,
-                bgColor: "rgb(180, 180, 180)",
-                text: "ABC",
-                fontSize: 50,
-                fontFamily: "'ヒラギノ角ゴ Pro W3', 'Hiragino Kaku Gothic Pro', 'メイリオ', 'Meiryo', 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif",
-            });
+            param = (param || {}).$safe(tm.ui.FlatButton.defaults);
 
-            this.superInit({
-                width: param.width,
-                height: param.height,
-            });
-
-            this.canvas.clearColor(param.bgColor);
+            this.superInit(param);
 
             this.setInteractive(true);
             this.setBoundingType("rect");
+            this.on("pointingend", function() {
+                this.flare('push');
+            });
 
             this.label = tm.display.Label(param.text).addChildTo(this);
             this.label.setFontSize(param.fontSize).setFontFamily(param.fontFamily).setAlign("center").setBaseline("middle");
         },
     });
 
-
+    tm.ui.FlatButton.defaults = {
+        width: 300,
+        height: 100,
+        fillStyle: "hsl(180, 60%, 50%)",
+        strokeStyle: "transparent",
+        text: "START",
+        fontSize: 50,
+        cornerRadius: 8,
+        fontFamily: "'ヒラギノ角ゴ Pro W3', 'Hiragino Kaku Gothic Pro', 'メイリオ', 'Meiryo', 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif",
+    };
 
 })();
 
@@ -17240,7 +17289,7 @@ tm.ui = tm.ui || {};
                         init: {
                             text: "Share",
                             width: 200,
-                            bgColor: "hsl(240, 100%, 64%)",
+                            fillStyle: "hsl(240, 100%, 64%)",
                         },
                         x: this._toGridX(4),
                         y: this._toGridY(9),
@@ -17250,7 +17299,7 @@ tm.ui = tm.ui || {};
                         init: {
                             text: "Back",
                             width: 200,
-                            bgColor: "hsl(240, 80%, 0%)",
+                            fillStyle: "hsl(240, 80%, 0%)",
                         },
                         x: this._toGridX(8),
                         y: this._toGridY(9),
